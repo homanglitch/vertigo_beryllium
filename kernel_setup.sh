Backup-Gits/kernel_xiaomi_sdm845-wip

kernel: Init drone ci
Change-Id: I6971558fa0f39eaf43f1fda731354c1b3b7ec6b5
Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

kernel_setup.sh
@@ -1,0 +1,33 @@
+#!/bin/bash
+#
+# Copyright (c) 2021 CloudedQuartz
+#
+
+# Script to set up environment to build an android kernel
+# Assumes required packages are already installed
+
+# Config
+CURRENT_DIR="$(pwd)"
+KERNELNAME="SilverCore"
+KERNEL_DIR="$CURRENT_DIR"
+AK_REPO="https://github.com/painkiller3/AnyKernel3"
+AK_DIR="$HOME/AnyKernel3"
+TC_DIR="$HOME"
+# End Config
+
+# clone_tc - clones proton clang to TC_DIR
+clone_tc() {
+	git clone --depth=1 https://github.com/mvaisakh/gcc-arm64 $TC_DIR/arm64
+	git clone --depth=1 https://github.com/mvaisakh/gcc-arm  $TC_DIR/arm
+}
+
+# Clones anykernel
+clone_ak() {
+	git clone $AK_REPO $AK_DIR
+}
+# Actually do stuff
+clone_tc
+clone_ak
+
+# Run build script
+. ${CURRENT_DIR}/kernel_build.sh

homanglitch/vertigo_beryllium

kernel_setup.sh
@@ -1,0 +1,33 @@
+#!/bin/bash
+#
+# Copyright (c) 2021 CloudedQuartz
+#
+
+# Script to set up environment to build an android kernel
+# Assumes required packages are already installed
+
+# Config
+CURRENT_DIR="$(pwd)"
+KERNELNAME="Vertigo"
+KERNEL_DIR="$CURRENT_DIR"
+AK_REPO="https://github.com/homanglitch/AnyKernel3"
+AK_DIR="$HOME/AnyKernel3"
+TC_DIR="$HOME"
+# End Config
+
+# clone_tc - clones proton clang to TC_DIR
+clone_tc() {
+	git clone --depth=1 https://github.com/mvaisakh/gcc-arm64 $TC_DIR/arm64
+	git clone --depth=1 https://github.com/mvaisakh/gcc-arm  $TC_DIR/arm
+}
+
+# Clones anykernel
+clone_ak() {
+	git clone $AK_REPO $AK_DIR
+}
+# Actually do stuff
+clone_tc
+clone_ak
+
+# Run build script
+. ${CURRENT_DIR}/kernel_build.sh